da88b1ef478414cbcd9a75d9cfacc829334d09d6
Trys <trysmudford@gmail.com>
2014-03-19 21:11:49 +0000

    Place url collector

--- liff/routes/place.js
+++ liff/routes/place.js
@@ -3,6 +3,7 @@
  * GET place.
  */
 
-exports.place = function(req, res){
-  res.render('place', { title: 'Liff', place: req.params.p });
+exports.place = function(req, res) {
+  var place = ( req.params.place ) ? req.params.place : 'Liff';
+  res.render('place', { title: place, place: place });
 };
