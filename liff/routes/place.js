09774bc849887c3451737706c68cf2ef2d4434be
Trys <trysmudford@gmail.com>
2014-03-19 22:06:52 +0000

    Place fields extracted

--- liff/routes/place.js
+++ liff/routes/place.js
@@ -7,7 +7,7 @@ exports.place = function(db) {
   return function(req, res) {
     db.collection('places').find({ 'slug' : req.params.place.toLowerCase() }).toArray(function (err, items){
       var place = items[0];
-      res.render('place', { title: place.title, place: place.definition });
+      res.render('place', { place : place });
     });
   }
 };
