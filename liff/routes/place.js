326e6dd5decfd19815022e2aef10ea2ed04cfe1f
Trys <trysmudford@gmail.com>
2014-07-28 21:46:48 +0100

    JSON request to Map API

--- liff/routes/place.js
+++ liff/routes/place.js
@@ -7,6 +7,22 @@ exports.place = function(db) {
   return function(req, res) {
     db.collection('places').find({ 'slug' : req.params.place.toLowerCase() }).toArray(function (err, items){
       var place = items[0];
+      if ( place.lat === undefined ) {
+
+      	var apiKey = require('../../data/mapapikey.json'),
+      		request,
+      		url;
+
+      	url = 'http://dev.virtualearth.net/REST/v1/Locations?q=' + place.title + '&key=' + apiKey.key;
+      	request = require('request');
+
+      	request(url, function(err, resp, body) {
+      		console.log(err);
+      		console.log(resp);
+      		console.log(body);
+      	});
+
+      }
       res.render('place', { place : place });
     });
   }
