68fa5a3f96bb7d6e7ccc337303b25169734659f8
Trys <trysmudford@gmail.com>
2014-09-17 21:29:41 +0100

    Mapstraction added, map displayed.

---
+++ liff/public/js/map.js
@@ -0,0 +1,8 @@
+function load_map() {
+	var map = new mxn.Mapstraction('place-map', 'leaflet'),
+		mapElement = document.getElementById('place-map'),
+		lat = mapElement.getAttribute('data-lat'),
+		lng = mapElement.getAttribute('data-lng'),
+		latlon = new mxn.LatLonPoint(lat, lng);
+	map.setCenterAndZoom(latlon, 10);
+}
